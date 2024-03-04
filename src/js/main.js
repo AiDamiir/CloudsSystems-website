29d5259ccb91f9eba0439ca23f147af846e3b809
AiDamiir <160788502+AiDamiir@users.noreply.github.com>
2024-03-04 17:31:47 +0300

    Add files via upload

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,11 @@
+import axios from 'axios'
 import 'macro-css'
 import '../scss/style.scss'
 import { particlesJs } from './modules/particlesJs'
 
 document.addEventListener('DOMContentLoaded', () => {
+	const menuNode = document.getElementById('menu')
+	const menuIcon = document.getElementById('menuIcon')
 	const navLinks = document.querySelectorAll('#navLink')
 	const showMoreSkillsButton = document.getElementById('showMoreButton')
 	const showMoreProjectsButton = document.getElementById(
@@ -12,10 +15,6 @@ document.addEventListener('DOMContentLoaded', () => {
 		'.key-skills__item:nth-child(n+9)'
 	)
 	const hiddenProjects = document.querySelectorAll('.project:nth-child(n+7)')
-	const stackButtons = document.querySelectorAll('.stack-button')
-	const stackContents = document.querySelectorAll('.stack-content')
-	const menuNode = document.getElementById('menu')
-
 	let isExpanded = false
 
 	function handleShowMoreSkillsButtonClick() {
@@ -32,39 +31,85 @@ document.addEventListener('DOMContentLoaded', () => {
 		isExpanded = !isExpanded
 	}
 
-	function handleStackButtonClick(event) {
-		const stackButton = event.target.closest('.stack-button')
-		if (stackButton) {
-			stackButtons.forEach((btn) => {
-				if (btn !== stackButton) {
-					btn.classList.remove('show')
-				}
-			})
-			stackButton.classList.toggle('show')
-			const stackContent = stackButton.nextElementSibling
-			stackContents.forEach((el) => {
-				el.style.maxHeight =
-					el === stackContent && !stackContent.style.maxHeight
-						? stackContent.scrollHeight + 'px'
-						: null
-			})
-		}
-	}
-
-	function handleMenuIconClick() {
+	function handleMenuClick() {
 		menuNode.classList.toggle('show')
+		menuIcon.classList.toggle('active')
 		document.documentElement.classList.toggle('lock')
 	}
 
-	function handleMenuLink() {
+	function handleMenuLinkClick() {
 		menuNode.classList.remove('show')
+		menuIcon.classList.remove('active')
 		document.documentElement.classList.remove('lock')
 	}
 
-	navLinks.forEach((navLink) => {
-		navLink.addEventListener('click', handleMenuLink)
-	})
+	function openPopup() {
+		popup.classList.toggle('hidden')
+		document.documentElement.classList.toggle('lock')
+	}
 
+	function closePopup() {
+		popup.classList.remove('hidden')
+		document.documentElement.classList.toggle('lock')
+	}
+
+	async function validateForm() {
+		const email = document.getElementById('contactFormEmail').value.trim()
+		const name = document.getElementById('contactFormUserName').value.trim()
+		const telephone = document.getElementById('contactFormPhone').value.trim()
+		const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+		if (!email || !name || !telephone) {
+			document.getElementById('popupMessage').innerHTML =
+				'Пожалуйста, заполните все поля формы.'
+			openPopup()
+			return false
+		}
+
+		if (!emailPattern.test(email)) {
+			document.getElementById('popupMessage').innerHTML =
+				'Пожалуйста, введите корректный адрес электронной почты.'
+			openPopup()
+			return false
+		}
+
+		return true
+	}
+
+	async function send(event) {
+		event.preventDefault()
+
+		if (!(await validateForm())) {
+			return
+		}
+
+		const email = document.getElementById('contactFormEmail').value.trim()
+		const name = document.getElementById('contactFormUserName').value.trim()
+		const telephone = document.getElementById('contactFormPhone').value.trim()
+		const data = {
+			email: email,
+			name: name,
+			telephone: telephone,
+		}
+
+		try {
+			const response = await axios.post('YOUR_API_ENDPOINT', data)
+			console.log(response)
+			document.getElementById('popupMessage').innerHTML =
+				'Данные успешно отправлены.'
+			openPopup()
+		} catch (error) {
+			console.error(error)
+			document.getElementById('popupMessage').innerHTML =
+				'Произошла ошибка при отправке данных. Пожалуйста, попробуйте снова позже.'
+			openPopup()
+		}
+	}
+
+	document.getElementById('closePopup').addEventListener('click', closePopup)
+	navLinks.forEach((navLink) => {
+		navLink.addEventListener('click', handleMenuLinkClick)
+	})
 	showMoreSkillsButton.addEventListener(
 		'click',
 		handleShowMoreSkillsButtonClick
@@ -73,10 +118,10 @@ document.addEventListener('DOMContentLoaded', () => {
 		'click',
 		handleShowMoreProjectsButtonClick
 	)
-	document.addEventListener('click', handleStackButtonClick)
 	document
-		.getElementById('menuIcon')
-		.addEventListener('click', handleMenuIconClick)
+		.getElementById('handleMenu')
+		.addEventListener('click', handleMenuClick)
+	document.getElementById('sendForm').addEventListener('click', send)
 
 	const mySwiper = new Swiper('.swiper-container', {
 		slidesPerView: 'auto',
